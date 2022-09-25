d4b1cfd56119fcc6da197f743dcc7d2deb240d33
KTakev <kostadin.takev@gmail.com>
2022-09-25 12:26:59 +0300

    adding PostMapping to UserLoginController

--- mobilelele/src/main/java/bg/softuni/mobilelele/web/UserLoginController.java
+++ mobilelele/src/main/java/bg/softuni/mobilelele/web/UserLoginController.java
@@ -1,7 +1,9 @@
 package bg.softuni.mobilelele.web;
 
+import bg.softuni.mobilelele.model.dto.UserLoginDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserLoginController {
@@ -9,5 +11,12 @@ public class UserLoginController {
     @GetMapping("/users/login")
     public String login() {
         return "auth-login";
+    }
+
+    @PostMapping("/user/login")
+    public String login(UserLoginDto userLoginDto) {
+
+        System.out.println(userLoginDto);
+        return "redirect:/";
     }
 }
