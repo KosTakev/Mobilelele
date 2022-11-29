a26b2fc0ebeb9ea6a6002110fca173307873b6b9
KTakev <kostadin.takev@gmail.com>
2022-11-29 15:59:43 +0200

    adding methods to OfferController, making dropdown menu on engine with EngineEnum

---
+++ mobilelele/src/main/java/bg/softuni/mobilelele/service/OfferService.java
@@ -0,0 +1,12 @@
+package bg.softuni.mobilelele.service;
+
+import bg.softuni.mobilelele.model.dto.AddOfferDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferService {
+
+    public void addOffer(AddOfferDto addOfferDto) {
+        //TODO
+    }
+}
